fdd6dca4b8269b21709bb1a14f639e9552a485c5
Ben Harris <benhdev@gmail.com>
2016-11-05 18:05:34 -0500

    remove credentials from ow

--- cloud/openwhisk.js
+++ cloud/openwhisk.js
@@ -2,13 +2,9 @@
 
 var openwhisk = require('openwhisk');
 
+var ow = openwhisk(require('./ow_cred.json'))
 
 
-var ow = openwhisk({
-  api: 'https://openwhisk.ng.bluemix.net/api/v1/',
-  api_key: 'aa933a0a-8bb7-447d-bc68-012bb2bbbec8:Jv9qZXNneht10ooJhqMIiHQv9M1qSnqOX3ti0MeEX5eGJ3jSi48ZPjURTBaq4Qvm',
-  namespace: 'Cloud Competitive Analysis_cca-test'
-})
 
 //list resources
 ow.actions.list().then(function (data) {
