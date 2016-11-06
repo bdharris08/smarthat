442ebbfe6c8b5b68a778eba4be933c3383b3b162
Ben Harris <benhdev@gmail.com>
2016-11-06 10:27:13 -0600

    Take image and play speech

---
+++ picToSpeech/itos.js
@@ -0,0 +1,37 @@
+//ITOS - Image to Speak
+// - Assuming image in this folder,
+// - send to google to get ocr
+// - receive text back
+// - play it using espeak
+
+const vision = require('@google-cloud/vision');
+const config = require('./config/config');
+const exec = require('child_process').exec;
+
+const img = process.argv[2];
+const espeak = `espeak -s 150 -v en` //ubuntu
+const say = `say -v Fred` //osx
+
+const visionClient = vision({
+  projectId: config.projectId,
+  keyFilename: './config/keyfile.json'
+});
+
+const imgToSpeech = function(img) {
+  visionClient.detectText(img).then((data) => {
+    const text = data[0][0].replace(/(\n)/gm, ' ');
+    //console.log(data)
+    console.log(`text ${text}`);
+    exec(`${espeak} ${text}`, (err, stdout, stderr) => {
+      if (stderr) {
+        console.log(stderr)
+      }
+    })
+  }).catch((err) => {
+    console.log(err);
+  })
+};
+
+imgToSpeech(img)
+
+module.export = imgToSpeech;
