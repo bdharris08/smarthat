cc9b246e91ae88c9e6ef6d6de02634e21fa6f74b
Clement Ho <ClemMakesApps@gmail.com>
2016-11-05 19:21:46 -0500

    Add buzzer iterator

--- buzzer/index.js
+++ buzzer/index.js
@@ -8,11 +8,17 @@ console.log(port);
 myLed.dir(m.DIR_OUT); //set the gpio direction to output
 var ledState = true; //Boolean to hold the state of Led
 
+var iterator = 0;
+
 function periodicActivity()
 {
   myLed.write(ledState?1:0); //if ledState is true then write a '1' (high) otherwise write a '0' (low)
   ledState = !ledState; //invert the ledState
-  setTimeout(periodicActivity, process.argv[3] || 1000); //call the indicated function after 1 second (1000 milliseconds)
+  iterator++;
+
+  if (iterator < 10) {
+    setTimeout(periodicActivity, process.argv[3] || 1000); //call the indicated function after 1 second (1000 milliseconds)
+  }
 }
 
 periodicActivity(); //call the periodicActivity function
